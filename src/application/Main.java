ed5333ccf1a54e754408d90d56aa92f678c402aa
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-14 15:12:23 -0300

    Create Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
-        System.out.println(department);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, department);
+        System.out.println(seller);
     }
 }
