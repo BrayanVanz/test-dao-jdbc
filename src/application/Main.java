d6a834298fc36108fc10dbe38889f786368c3ff4
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-14 15:28:40 -0300

    Create SellerDaoJDCB and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
